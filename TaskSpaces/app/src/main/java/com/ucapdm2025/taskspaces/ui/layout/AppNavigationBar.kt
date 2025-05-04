cdc743dba01fcc5a21a8a07f2aa8bb12578f56c0
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-03 20:16:59 -0600

    Add colors to navigation bar

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/AppNavigationBar.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/AppNavigationBar.kt
@@ -8,22 +8,28 @@ import androidx.compose.material.icons.outlined.Bookmark
 import androidx.compose.material.icons.outlined.BookmarkBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.ucapdm2025.taskspaces.ui.navigation.BookmarksRoute
 import com.ucapdm2025.taskspaces.ui.navigation.HomeRoute
 import com.ucapdm2025.taskspaces.ui.navigation.SearchRoute
+import com.ucapdm2025.taskspaces.ui.theme.Black25
 import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 
 // A data class representing a navigation item in the navigation bar.
@@ -64,16 +70,20 @@ fun AppNavigationBar(navController: NavHostController) {
 
     var selectedItem by rememberSaveable { mutableIntStateOf(0) }
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+    ) {
         navItems.forEachIndexed { index, item ->
             val isSelected = selectedItem == index
 
             NavigationBarItem(
-                label = { Text(item.label) },
+                label = { Text(text = item.label) },
                 icon = {
                     Icon(
                         imageVector = if (isSelected) item.filledIcon else item.outlinedIcon,
-                        contentDescription = item.label
+                        contentDescription = item.label,
+                        tint = MaterialTheme.colorScheme.onPrimary
                     )
                 },
                 selected = isSelected,
@@ -85,6 +95,15 @@ fun AppNavigationBar(navController: NavHostController) {
                         restoreState = true
                     }
                 },
+                colors = NavigationBarItemColors(
+                    selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                    selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                    selectedIndicatorColor = Black25,
+                    unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                    unselectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                    disabledIconColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f),
+                    disabledTextColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f),
+                ),
             )
         }
     }
