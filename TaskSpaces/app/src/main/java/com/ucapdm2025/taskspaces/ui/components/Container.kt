4fb1eda1ce1f732240b32e02a8423031356edfb9
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-16 16:18:11 -0600

    Set container background color based on current theme

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/components/Container.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/components/Container.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedTheme
 import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 
 /**
@@ -27,20 +29,41 @@ fun Container(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = Color(0x0D21232B),
+                color = ExtendedTheme.colors.background05,
                 shape = RoundedCornerShape(size = 24.dp)
             )
-            .border(width = 1.dp, color = Color(0x4021232B), shape = RoundedCornerShape(size = 24.dp))
+            .border(
+                width = 1.dp,
+                color = ExtendedTheme.colors.background25,
+                shape = RoundedCornerShape(size = 24.dp)
+            )
             .padding(24.dp)
     ) { content() }
 }
 
+// Light theme preview
+//@Preview(showBackground = true)
+//@Composable
+//fun ContainerPreview() {
+//    TaskSpacesTheme(darkTheme = false) {
+//        ExtendedColors(darkTheme = false) {
+//            Container {
+//                Text(text = "Container Preview", color = MaterialTheme.colorScheme.onSurface)
+//            }
+//        }
+//    }
+//}
+
+// Dark theme preview
 @Preview(showBackground = true, backgroundColor = 0xFF000000)
 @Composable
 fun ContainerPreview() {
     TaskSpacesTheme(darkTheme = true) {
-        Container {
-            Text(text = "Container Preview", color = MaterialTheme.colorScheme.onSurface)
+        ExtendedColors(darkTheme = true) {
+            Container {
+                Text(text = "Container Preview", color = MaterialTheme.colorScheme.onSurface)
+            }
         }
     }
 }
+
