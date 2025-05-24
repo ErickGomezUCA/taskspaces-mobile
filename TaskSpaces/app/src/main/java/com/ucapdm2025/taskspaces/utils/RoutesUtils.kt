9bfbfa57b7a225264034105b01c99dcfe3e9af64
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-24 16:38:35 -0600

    Document getCurrentRoute utility function

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/utils/RoutesUtils.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/utils/RoutesUtils.kt
@@ -2,6 +2,12 @@ package com.ucapdm2025.taskspaces.utils
 
 import androidx.navigation.NavBackStackEntry
 
+/**
+ * Utility function to get the current route from the NavBackStackEntry.
+ *
+ * @param navBackStackEntry The NavBackStackEntry from which to extract the current route.
+ * @return The current route as a String, or null if not available.
+ */
 fun getCurrentRoute(navBackStackEntry: NavBackStackEntry?): String? {
     return navBackStackEntry?.destination?.route?.split(".")?.last()
 }
