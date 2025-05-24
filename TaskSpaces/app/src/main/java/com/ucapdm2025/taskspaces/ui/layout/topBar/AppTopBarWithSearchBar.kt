0695e322c403243cfb491a452064f447a1c87e6d
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-24 15:50:44 -0600

    Update AppTopBarWithSearchBar to include more placeholder and search action parameters

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/topBar/AppTopBarWithSearchBar.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/topBar/AppTopBarWithSearchBar.kt
@@ -15,19 +15,33 @@ import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
 import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 
 /**
- * Top bar for the app, used in SearchScreen and BookmarksScreen.
+ * Top bar for the app, used in all screens.
  * @param query The current text value of the search bar.
+ * @param placeholder The placeholder text displayed when the search bar is empty.
  * @param onQueryChange A callback invoked when the text in the search bar changes.
+ * @param onSearch A callback invoked when the user submits a search action.
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppTopBarWithSearchBar(query: String, onQueryChange: (String) -> Unit) {
+fun AppTopBarWithSearchBar(
+    query: String,
+    placeholder: String = "Placeholder...",
+    onQueryChange: (String) -> Unit,
+    onSearch: (String) -> Unit
+) {
     TopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.background,
             titleContentColor = MaterialTheme.colorScheme.onBackground,
         ),
-        title = { SearchBar(query = query, onQueryChange = onQueryChange) },
+        title = {
+            SearchBar(
+                query = query,
+                placeholder = placeholder,
+                onQueryChange = onQueryChange,
+                onSearch = onSearch
+            )
+        },
         navigationIcon = {
 //            TODO: Show chevron only when stack is one route ahead of SearchRoute or BookmarksRoute
             IconButton(onClick = { /* TODO: Handle back navigation */ }) {
@@ -46,7 +60,17 @@ fun AppTopBarWithSearchBar(query: String, onQueryChange: (String) -> Unit) {
 fun AppTopBarWithSearchBarLightPreview() {
     TaskSpacesTheme {
         ExtendedColors {
-            AppTopBarWithSearchBar(query = "Search", onQueryChange = {})
+            AppTopBarWithSearchBar(query = "", placeholder = "Search...", onQueryChange = {}, onSearch = {})
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppTopBarWithSearchBarWithValueLightPreview() {
+    TaskSpacesTheme {
+        ExtendedColors {
+            AppTopBarWithSearchBar(query = "Query value", placeholder = "Search...", onQueryChange = {}, onSearch = {})
         }
     }
 }
@@ -56,7 +80,17 @@ fun AppTopBarWithSearchBarLightPreview() {
 fun AppTopBarWithSearchBarDarkPreview() {
     TaskSpacesTheme(darkTheme = true) {
         ExtendedColors(darkTheme = true) {
-            AppTopBarWithSearchBar(query = "Search", onQueryChange = {})
+            AppTopBarWithSearchBar(query = "", placeholder = "Search tasks...", onQueryChange = {}, onSearch = {})
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppTopBarWithSearchBarWithValueDarkPreview() {
+    TaskSpacesTheme(darkTheme = true) {
+        ExtendedColors(darkTheme = true) {
+            AppTopBarWithSearchBar(query = "Query value", placeholder = "Search tasks...", onQueryChange = {}, onSearch = {})
         }
     }
 }
