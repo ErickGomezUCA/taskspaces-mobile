04b234ccc49f73551d40442a5bb2dbd7709050a4
DanieManzanares <000151316@uca.edu.sv>
2025-05-24 23:46:23 -0600

    Workspaces shared with me section added to Home screen

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/HomeScreen.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/HomeScreen.kt
@@ -1,36 +1,42 @@
-
 package com.ucapdm2025.taskspaces.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import com.ucapdm2025.taskspaces.ui.components.projects.SharedWorkspacesSection
 import com.ucapdm2025.taskspaces.ui.components.projects.YourWorkspacesSection
 import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
 import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
-import androidx.compose.ui.unit.dp
 
-
-// TODO: Implement the HomeScreen UI
-/**
- * A composable function that displays the home screen.
- * Currently, it shows a simple text message.
- *
- * @param modifier A [Modifier] for customizing the layout or behavior of the home screen.
- */
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        //Text("Welcome, \$USER", style = MaterialTheme.typography.titleLarge)
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        item {
+            Text(
+                text = "Welcome, \$USER",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
 
-        Spacer(modifier = Modifier.height(12.dp))
+        item {
+            YourWorkspacesSection()
+            Spacer(modifier = Modifier.height(16.dp))
+        }
 
-        YourWorkspacesSection()
+        item {
+            SharedWorkspacesSection()
+            Spacer(modifier = Modifier.height(80.dp)) // para evitar que el último botón quede tapado por la bottom bar
+        }
     }
 }
 
