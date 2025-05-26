7f8f7d44161e363203e735b7377d33c5d38b272c
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-26 11:27:28 -0600

    Implement project repository in SearchViewModel

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/search/SearchViewModel.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/search/SearchViewModel.kt
@@ -2,6 +2,8 @@ package com.ucapdm2025.taskspaces.ui.screens.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ucapdm2025.taskspaces.data.repository.project.ProjectRepository
+import com.ucapdm2025.taskspaces.data.repository.project.ProjectRepositoryImpl
 import com.ucapdm2025.taskspaces.data.repository.workspace.WorkspaceRepository
 import com.ucapdm2025.taskspaces.data.repository.workspace.WorkspaceRepositoryImpl
 import kotlinx.coroutines.flow.SharingStarted
@@ -9,9 +11,10 @@ import kotlinx.coroutines.flow.stateIn
 
 class SearchViewModel: ViewModel() {
     private val workspaceRepository: WorkspaceRepository = WorkspaceRepositoryImpl()
+    private val projectRepository: ProjectRepository = ProjectRepositoryImpl()
 
     val workspaces = workspaceRepository.getWorkspaces().stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
-//    val projects
+    val projects = projectRepository.getProjects().stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 //    val tasks
 //    val users
 }
