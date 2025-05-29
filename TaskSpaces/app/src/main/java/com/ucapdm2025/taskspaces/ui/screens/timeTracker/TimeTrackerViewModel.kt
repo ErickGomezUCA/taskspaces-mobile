6b4ec28d5d26fc3df4b04b09adb701ab704e2588
ErickGomezUCA <00300723@uca.edu.sv>
2025-05-29 16:57:09 -0600

    Implement Time Tracker ViewModel

---
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/timeTracker/TimeTrackerViewModel.kt
@@ -0,0 +1,39 @@
+package com.ucapdm2025.taskspaces.ui.screens.timeTracker
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ucapdm2025.taskspaces.data.model.Task
+import com.ucapdm2025.taskspaces.data.repository.task.TaskRepository
+import com.ucapdm2025.taskspaces.data.repository.task.TaskRepositoryImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class TimeTrackerViewModel: ViewModel() {
+    private val taskRepository: TaskRepository = TaskRepositoryImpl()
+    val id = 1;
+
+    private val _task = MutableStateFlow<Task>(Task(
+        id = 0,
+        title = "",
+        description = "",
+        date = "",
+        timer = 0,
+        status = "PENDING",
+        tags = emptyList(),
+        assignedMembers = emptyList(),
+        createdAt = "",
+        updatedAt = ""
+    ))
+    val task: StateFlow<Task> = _task
+
+    init {
+        viewModelScope.launch {
+            _task.value = taskRepository.getTaskById(id)!!
+        }
+    }
+
+    fun getTAskTimer(): Int {
+        return task.value.timer
+    }
+}
