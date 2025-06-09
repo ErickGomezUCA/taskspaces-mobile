ff3a8eb89235fa38fab265856d23fe4a85b6e3ed
Katya Herrera <00188119@uca.edu.sv>
2025-06-09 13:35:10 -0600

    Replace DoingStatusColor and DoneStatusColor with Color.Red and Color.Blue

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/BookmarksScreen.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/BookmarksScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ucapdm2025.taskspaces.ui.components.general.Container
@@ -141,15 +142,15 @@ fun BookmarkList(bookmarks: List<Task>) {
         Task(
             title = "Task Title",
             tags = listOf(
-                Tag("Tag", DoingStatusColor),
-                Tag("Tag", DoneStatusColor)
+                Tag("Tag", Color.Red),
+                Tag("Tag", Color.Blue)
             )
         ),
         Task(
             title = "Task Title",
             tags = listOf(
-                Tag("Tag", DoingStatusColor),
-                Tag("Tag", DoneStatusColor)
+                Tag("Tag", Color.Red),
+                Tag("Tag", Color.Blue)
             )
         )
     )
