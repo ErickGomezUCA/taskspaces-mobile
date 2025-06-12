3abba73bd087f290d9547af40684f3ff99d3c6d8
DanieManzanares <000151316@uca.edu.sv>
2025-06-12 17:58:07 -0600

    Preview added to Shared Work Space Section

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/Home/SharedWorkspacesSection.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/Home/SharedWorkspacesSection.kt
@@ -4,9 +4,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
 import com.ucapdm2025.taskspaces.ui.theme.ExtendedTheme
+import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 
+/**
+ * A section that displays workspaces shared with the user.
+ *
+ * Includes two sample [WorkspaceCard] components and a "See more" button.
+ */
 @Composable
 fun SharedWorkspacesSection(modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxWidth()) {
@@ -26,6 +34,36 @@ fun SharedWorkspacesSection(modifier: Modifier = Modifier) {
             shape = MaterialTheme.shapes.medium
         ) {
             Text("See more")
+        }
+    }
+}
+
+/**
+ * Preview of [SharedWorkspacesSection] in light mode.
+ */
+@Preview(showBackground = true)
+@Composable
+fun SharedWorkspacesSectionPreviewLight() {
+    TaskSpacesTheme(darkTheme = false) {
+        ExtendedColors(darkTheme = false) {
+            Surface {
+                SharedWorkspacesSection()
+            }
+        }
+    }
+}
+
+/**
+ * Preview of [SharedWorkspacesSection] in dark mode.
+ */
+@Preview(showBackground = true, backgroundColor = 0xFF27272A)
+@Composable
+fun SharedWorkspacesSectionPreviewDark() {
+    TaskSpacesTheme(darkTheme = true) {
+        ExtendedColors(darkTheme = true) {
+            Surface {
+                SharedWorkspacesSection()
+            }
         }
     }
 }
