a3bb6d4a6f0e4a47e16f589028b7014f4b4e5830
ErickGomezUCA <00300723@uca.edu.sv>
2025-06-13 14:22:46 -0600

    Create auth data store to save login tokens

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/TaskSpacesApplication.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/TaskSpacesApplication.kt
@@ -2,15 +2,26 @@ package com.ucapdm2025.taskspaces
 
 import android.app.Application
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.ucapdm2025.taskspaces.data.AppProvider
+import com.ucapdm2025.taskspaces.data.repository.auth.AuthRepository
+
+private const val AUTH_TOKEN_NAME = "auth_token"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = AUTH_TOKEN_NAME)
 
 class TaskSpacesApplication: Application() {
+    lateinit var authRepository: AuthRepository
+
     val appProvider by lazy {
         AppProvider(this)
     }
 
     override fun onCreate() {
         super.onCreate()
+
+        authRepository = AuthRepository(dataStore)
 
         val workspaceRepository = appProvider.provideWorkspaceRepository()
         val userRepository = appProvider.provideUserRepository()
---
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/data/repository/auth/AuthRepository.kt
@@ -0,0 +1,39 @@
+package com.ucapdm2025.taskspaces.data.repository.auth
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import okio.IOException
+
+class AuthRepository(
+    private val dataStore: DataStore<Preferences>
+) {
+    private companion object {
+        val AUTH_TOKEN = stringPreferencesKey("auth_token")
+    }
+
+    val authToken: Flow<String> = dataStore.data.catch { error ->
+        if (error is IOException) {
+            emit(emptyPreferences())
+
+        } else {
+            throw error
+        }
+    }
+        .map { preferences ->
+            preferences[AUTH_TOKEN] ?: ""
+        }
+
+    suspend fun saveAuthToken(
+        token: String
+    ) {
+        dataStore.edit { preferences ->
+            preferences[AUTH_TOKEN] = token
+        }
+    }
+}
