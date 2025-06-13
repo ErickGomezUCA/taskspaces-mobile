fa783157dde59522266796fb93109824e3c941d6
DanieManzanares <000151316@uca.edu.sv>
2025-06-12 18:00:43 -0600

    Previw added to Your work space Section

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/Home/YourWorkspacesSection.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/Home/YourWorkspacesSection.kt
@@ -5,10 +5,19 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
 import com.ucapdm2025.taskspaces.ui.theme.ExtendedTheme
+import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 
+/**
+ * A section that displays the user's own workspaces.
+ *
+ * Includes two [WorkspaceCard]s, a centered "Create workspace +" text button,
+ * and a primary-colored "See more" button.
+ */
 @Composable
 fun YourWorkspacesSection(
     modifier: Modifier = Modifier
@@ -48,6 +57,36 @@ fun YourWorkspacesSection(
             shape = MaterialTheme.shapes.medium
         ) {
             Text("See more")
+        }
+    }
+}
+
+/**
+ * Preview of [YourWorkspacesSection] in light theme.
+ */
+@Preview(showBackground = true)
+@Composable
+fun YourWorkspacesSectionPreviewLight() {
+    TaskSpacesTheme(darkTheme = false) {
+        ExtendedColors(darkTheme = false) {
+            Surface {
+                YourWorkspacesSection()
+            }
+        }
+    }
+}
+
+/**
+ * Preview of [YourWorkspacesSection] in dark theme.
+ */
+@Preview(showBackground = true, backgroundColor = 0xFF27272A)
+@Composable
+fun YourWorkspacesSectionPreviewDark() {
+    TaskSpacesTheme(darkTheme = true) {
+        ExtendedColors(darkTheme = true) {
+            Surface {
+                YourWorkspacesSection()
+            }
         }
     }
 }
