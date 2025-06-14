be0100082a88af01a0af7138a0f5ca36914f8af1
Katya Herrera <00188119@uca.edu.sv>
2025-06-14 16:21:48 -0600

    Add: container for SearchScreen

---
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/components/general/SearchContainer.kt
@@ -0,0 +1,110 @@
+package com.ucapdm2025.taskspaces.ui.components.general
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedColors
+import com.ucapdm2025.taskspaces.ui.theme.ExtendedTheme
+import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
+
+@Composable
+fun SearchContainer(
+    title: String,
+    modifier: Modifier = Modifier,
+    onViewMoreClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = ExtendedTheme.colors.background05,
+                shape = RoundedCornerShape(size = 24.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = ExtendedTheme.colors.background25,
+                shape = RoundedCornerShape(size = 24.dp)
+            )
+            .padding(24.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(0.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            TextButton(onClick = onViewMoreClick) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "View more",
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Icon(
+                        imageVector = Icons.Default.ChevronRight,
+                        contentDescription = "Go to more",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        content()
+    }
+}
+
+
+/**
+ * A preview composable for the [Container] component.
+ *
+ * Displays a sample container with a title and content, allowing developers to visualize the layout and styling during design time.
+ */
+@Preview(showBackground = true)
+@Composable
+fun SearchContainerPreviewLightMode() {
+    TaskSpacesTheme(darkTheme = false) {
+        ExtendedColors(darkTheme = false) {
+            SearchContainer(title = "Container Title") {
+                Text(text = "Container Preview", color = MaterialTheme.colorScheme.onSurface)
+            }
+        }
+    }
+}
+
+/**
+ * A preview composable for the [Container] component in dark mode.
+ *
+ * Displays a sample container with a title and content, allowing developers to visualize the layout and styling during design time in dark mode.
+ */
+@Preview(showBackground = true, backgroundColor = 0xFF27272A)
+@Composable
+fun SearchContainerPreviewDarkMode() {
+    TaskSpacesTheme(darkTheme = true) {
+        ExtendedColors(darkTheme = true) {
+            SearchContainer(title = "Container Title") {
+                Text(text = "Container Preview", color = MaterialTheme.colorScheme.onSurface)
+            }
+        }
+    }
+}
+
