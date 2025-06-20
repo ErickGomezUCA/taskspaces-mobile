d8b816989d4e15d3a07964f90e7930a84543e610
ErickGomezUCA <00300723@uca.edu.sv>
2025-06-20 17:21:06 -0600

    Separate show members and invite member

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/components/workspace/ManageMembersDialog.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/components/workspace/ManageMembersDialog.kt
@@ -1,7 +1,5 @@
 package com.ucapdm2025.taskspaces.ui.components.workspace
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,13 +13,15 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -32,7 +32,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ucapdm2025.taskspaces.data.model.UserModel
@@ -48,71 +47,111 @@ fun ManageMembersDialog(
 ) {
     var expanded by remember { mutableStateOf(false) }
     var selectedRole by remember { mutableStateOf<MemberRoles>(MemberRoles.READER) }
+    var username by remember { mutableStateOf("") }
 
     AlertDialog(
         onDismissRequest = onDismissRequest,
         title = { Text(text = "Manage members") },
         text = {
             Column(modifier = Modifier.fillMaxWidth()) {
-                Column(modifier = Modifier.fillMaxWidth()) {
-                    members.forEach { member ->
-                        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                            Box(
-                                modifier = Modifier
-                                    .size(24.dp)
-                                    .background(Color(0xFFFFA726), CircleShape)
-                            )
-
-                            Text(text = member.username, modifier = Modifier.weight(1f))
-
-                            ExposedDropdownMenuBox(
-                                expanded = expanded,
-                                onExpandedChange = { expanded = !expanded },
-                                modifier = Modifier.padding(horizontal = 8.dp)
+                Column(modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp)) {
+                    if (members.isEmpty()) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "No members in this workspace")
+                        }
+                    } else {
+                        members.forEach { member ->
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
                             ) {
-                                TextButton(
-                                    onClick = { expanded = true }
-                                ) {
-                                    Text(selectedRole.toString())
-                                }
-                                ExposedDropdownMenu(
+                                Box(
+                                    modifier = Modifier
+                                        .size(24.dp)
+                                        .background(Color(0xFFFFA726), CircleShape)
+                                )
+
+                                Text(text = member.username, modifier = Modifier.weight(1f))
+
+                                ExposedDropdownMenuBox(
                                     expanded = expanded,
-                                    onDismissRequest = { expanded = false }
+                                    onExpandedChange = { expanded = !expanded },
+                                    modifier = Modifier.padding(horizontal = 8.dp)
                                 ) {
-                                    MemberRoles.entries.forEach { role ->
-                                        DropdownMenuItem(
-                                            text = { Text(role.toString()) },
-                                            onClick = {
-                                                selectedRole = role
-                                                expanded = false
+                                    TextButton(
+                                        onClick = { expanded = true }
+                                    ) {
+                                        Text(selectedRole.toString())
+                                    }
+                                    ExposedDropdownMenu(
+                                        expanded = expanded,
+                                        onDismissRequest = { expanded = false }
+                                    ) {
+                                        MemberRoles.entries.forEach { role ->
+                                            DropdownMenuItem(
+                                                text = { Text(role.toString()) },
+                                                onClick = {
+                                                    selectedRole = role
+                                                    expanded = false
 //                                                onRoleChange(member, role)
-                                            }
-                                        )
+                                                }
+                                            )
+                                        }
                                     }
                                 }
-                            }
 
-                            IconButton(onClick = {}) {
-                                Icon(
-                                    imageVector = Icons.Default.Delete,
-                                    contentDescription = "Remove member"
-                                )
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        imageVector = Icons.Default.Delete,
+                                        contentDescription = "Remove member"
+                                    )
+                                }
                             }
                         }
+
                     }
                 }
 
-                TextButton(onClick = { onDismissRequest() }, modifier = Modifier.fillMaxWidth()) {
-                    Text(text = "Add member +")
+                Divider(modifier = Modifier.padding(vertical = 16.dp))
+
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Text(
+                            text = "OR",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = ExtendedTheme.colors.background75
+                        )
+                        Text(text = "Invite a new member:")
+                    }
+
+                    OutlinedTextField(
+                        value = username,
+                        onValueChange = { username = it },
+                        label = { Text(text = "Username") })
+                }
+
+
+                TextButton(
+                    onClick = { onDismissRequest() },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp),
+                ) {
+                    Text(text = "Invite +")
                 }
             }
         },
         confirmButton = {
-            OutlinedButton(
+            Button(
                 onClick = { onDismissRequest() },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(8.dp),
-                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
             ) { Text(text = "Close") }
         },
     )
