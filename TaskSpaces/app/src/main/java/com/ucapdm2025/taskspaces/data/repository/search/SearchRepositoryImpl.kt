7e4c063b364f6ddc4e3cc01ef1169520f7dd1d8f
ErickGomezUCA <00300723@uca.edu.sv>
2025-06-19 22:16:37 -0600

    Implement SearchViewModel

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/data/repository/search/SearchRepositoryImpl.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/data/repository/search/SearchRepositoryImpl.kt
@@ -1,21 +1,89 @@
 package com.ucapdm2025.taskspaces.data.repository.search
 
 import com.ucapdm2025.taskspaces.data.database.dao.SearchDao
+import com.ucapdm2025.taskspaces.data.database.entities.toDomain
+import com.ucapdm2025.taskspaces.data.model.ProjectModel
 import com.ucapdm2025.taskspaces.data.model.SearchModel
+import com.ucapdm2025.taskspaces.data.model.TaskModel
+import com.ucapdm2025.taskspaces.data.model.WorkspaceModel
 import com.ucapdm2025.taskspaces.data.remote.services.SearchService
 import com.ucapdm2025.taskspaces.helpers.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 
 class SearchRepositoryImpl(
     private val searchDao: SearchDao,
     private val searchService: SearchService
-): SearchRepository {
+) : SearchRepository {
     override fun search(query: String): Flow<Resource<SearchModel>> = flow {
         emit(Resource.Loading)
 
-        try {
+//        Search locally only
+//        TODO: Find a way to search to server when connected to the internet
+//        and locally when offline
+        var localWorkspaces: List<WorkspaceModel> = emptyList()
+        var localProjects: List<ProjectModel> = emptyList()
+        var localTasks: List<TaskModel> = emptyList()
 
-        }
+        searchDao.searchWorkspaces(query).map { entities ->
+            localWorkspaces = entities.map { it.toDomain() }
+        }.distinctUntilChanged()
+
+        searchDao.searchProjects(query).map { entities ->
+            localProjects = entities.map { it.toDomain() }
+        }.distinctUntilChanged()
+
+        searchDao.searchTasks(query).map { entities ->
+            localTasks = entities.map { it.toDomain() }
+        }.distinctUntilChanged()
+
+        val search: Flow<Resource<SearchModel>> = flowOf(
+            Resource.Success(
+                SearchModel(
+                    workspaces = localWorkspaces,
+                    projects = localProjects,
+                    tasks = localTasks
+                )
+            )
+        )
+
+        emitAll(search)
+    }
+
+    override fun searchWorkspaces(query: String): Flow<Resource<List<WorkspaceModel>>> = flow {
+        emit(Resource.Loading)
+
+        val localWorkspaces = searchDao.searchWorkspaces(query).map { entities ->
+            val results = entities.map { it.toDomain() }
+            Resource.Success(results)
+        }.distinctUntilChanged()
+
+        emitAll(localWorkspaces)
+    }
+
+    override fun searchProjects(query: String): Flow<Resource<List<ProjectModel>>> = flow {
+        emit(Resource.Loading)
+
+        val localProjects = searchDao.searchProjects(query).map { entities ->
+            val results = entities.map { it.toDomain() }
+            Resource.Success(results)
+        }.distinctUntilChanged()
+
+        emitAll(localProjects)
+    }
+
+    override fun searchTasks(query: String): Flow<Resource<List<TaskModel>>> = flow {
+        emit(Resource.Loading)
+
+        val localTasks = searchDao.searchTasks(query).map { entities ->
+            val results = entities.map { it.toDomain() }
+            Resource.Success(results)
+        }.distinctUntilChanged()
+
+        emitAll(localTasks)
     }
 }
--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/search/SearchViewModel.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/search/SearchViewModel.kt
@@ -1,9 +1,57 @@
 package com.ucapdm2025.taskspaces.ui.screens.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.ucapdm2025.taskspaces.data.model.SearchModel
+import com.ucapdm2025.taskspaces.data.repository.search.SearchRepository
+import com.ucapdm2025.taskspaces.helpers.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
+/**
+ * ViewModel for managing search functionality in the application.
+ */
 class SearchViewModel(private val searchRepository: SearchRepository): ViewModel() {
+    private val _searchResults: MutableStateFlow<SearchModel?> = MutableStateFlow(null)
+    val searchResults = _searchResults.asStateFlow()
 
+    fun search(query: String) {
+        viewModelScope.launch {
+            searchRepository.search(query).collect { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        // Handle loading state if necessary
+                    }
+
+                    is Resource.Success -> {
+                        _searchResults.value = resource.data
+                    }
+
+                    is Resource.Error -> {
+                        // Handle error state if necessary
+                        _searchResults.value = null
+                    }
+                }
+            }
+        }
+    }
 }
 
-class SearchViewModelFactory
+/**
+ * Factory for creating instances of SearchViewModel.
+ *
+ * @param searchRepository The repository used to perform search operations.
+ */
+class SearchViewModelFactory(
+    private val searchRepository: SearchRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SearchViewModel(searchRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
