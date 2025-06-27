d8f503f70961020f0e7ad11564feb34bd84d3b6a
DanieManzanares <000151316@uca.edu.sv>
2025-06-27 00:43:06 -0600

    AppScaffold modified to show first name on the topBar

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/AppScaffold.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/layout/AppScaffold.kt
@@ -5,28 +5,36 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ucapdm2025.taskspaces.ui.layout.topBar.SelectAppTopBar
 import com.ucapdm2025.taskspaces.ui.navigation.AppNavigation
+import com.ucapdm2025.taskspaces.ui.screens.home.HomeViewModel
 import com.ucapdm2025.taskspaces.ui.theme.TaskSpacesTheme
 import com.ucapdm2025.taskspaces.utils.getCurrentRoute
 
-/**
- * A composable function that provides the main scaffold layout for the app.
- * Includes a bottom navigation bar and a content area for navigation.
- */
 @Composable
 fun AppScaffold() {
     val navController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val navBackStackEntry = navController.currentBackStackEntryAsState().value
     val currentRoute = getCurrentRoute(navBackStackEntry) ?: "UnknownRoute"
 
+    val viewModel: HomeViewModel = viewModel(factory = HomeViewModel.Factory)
+    val userNameState = viewModel.userName.collectAsStateWithLifecycle()
+    val firstName = userNameState.value.split(" ").firstOrNull() ?: "User"
+
     Scaffold(
-        topBar = { SelectAppTopBar(currentRoute = currentRoute, navController = navController) },
+        topBar = {
+            SelectAppTopBar(
+                currentRoute = currentRoute,
+                navController = navController,
+                firstName = firstName
+            )
+        },
         bottomBar = { AppNavigationBar(navController = navController) },
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
@@ -36,10 +44,6 @@ fun AppScaffold() {
     }
 }
 
-/**
- * A preview of the [AppScaffold] composable.
- * Demonstrates the scaffold with a default theme.
- */
 @Preview(showBackground = true)
 @Composable
 fun AppScaffoldLightPreview() {
