8036248854be08e5fc7a5437917193b86a2937fe
Katya Herrera <00188119@uca.edu.sv>
2025-06-27 21:49:47 -0600

    add loading state for auto-login using saved token

--- TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/login/LoginViewModel.kt
+++ TaskSpaces/app/src/main/java/com/ucapdm2025/taskspaces/ui/screens/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.ucapdm2025.taskspaces.ui.screens.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -9,8 +8,11 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.ucapdm2025.taskspaces.TaskSpacesApplication
 import com.ucapdm2025.taskspaces.data.repository.auth.AuthRepository
+import com.ucapdm2025.taskspaces.helpers.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -31,12 +33,29 @@ class LoginViewModel(
             started = SharingStarted.WhileSubscribed(5_000),
             initialValue = 0
         )
+    // UI state for login process (loading, success, error)
+    private val _loginState = MutableStateFlow<UiState<Unit>>(UiState.Success(Unit))
+    val loginState: StateFlow<UiState<Unit>> = _loginState.asStateFlow()
+
+    // Automatically trigger login flow if a valid token already exists.
+    // without needing to manually log in again.
+    init {
+        viewModelScope.launch {
+            authToken.collect { token ->
+                if (token.isNotEmpty()) {
+                    _loginState.value = UiState.Loading
+                }
+            }
+        }
+    }
 
     fun login(
         email: String,
         password: String
     ) {
         viewModelScope.launch {
+            _loginState.value = UiState.Loading
+
             val response = authRepository.login(email.trim(), password.trim())
 
             if (response.isSuccess) {
@@ -46,13 +65,11 @@ class LoginViewModel(
 
                 saveToken(token)
                 saveAuthUserId(userId)
+            // Simplified error handling: combine exception check and fallback message in one line
             } else {
                 // Handle login failure, e.g., show a message to the user
-                val exception = response.exceptionOrNull()
-                if (exception != null) {
-                    // Log or handle the exception as needed
-                    Log.d("test1", "Login failed: ${exception.message}")
-                }
+                val exception = response.exceptionOrNull()?.message ?: "Login failed"
+                    _loginState.value = UiState.Error(exception)
             }
         }
     }
